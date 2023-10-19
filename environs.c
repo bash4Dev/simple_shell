3d0b620b6f4b0250e04e30351d2a7e54a7ff4855
Bashir Umar Idris <idrisbashirumr@gmail.com>
2023-10-19 04:27:25 +0100

    Fix betty check error

--- environs.c
+++ environs.c
@@ -1,29 +1,6 @@
 #include "shell.h"
 
 /**
- * cmp_env - compares env variables names
- * with the name passed.
- * @nenv: name of the environment variable
- * @name: name passed
- *
- * Return: 0 if are not equal. Another value if they are.
- */
-int cmp_env(const char *nenv, const char *name)
-{
-	int i;
-	
-	for (i = 0; nenv[i] != '='; i++)
-	{
-		if (nenv[i] != name[i])
-		{
-			return (0);
-		}
-	}
-
-	return (i + 1);
-}
-
-/**
  * get_env - get an environment variable
  * @name: name of the environment variable
  * @environ: environment variable
